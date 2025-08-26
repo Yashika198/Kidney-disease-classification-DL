6ef51355af4d1af1fc986c9d5861ee626e50b3ec
Yashika198 <gyashika198@gmail.com>
2025-08-26 20:54:12 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from KIDNEY_DISEASE_Classifier import logger
-logger.info("Welcome")
+from KIDNEY_DISEASE_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
